7bc4d00a845d61bc5618ca2ad81ac6703917b6fa
diegoserraa <diegoserra120@hotmail.com>
2021-11-20 14:08:15 -0300

    ambiente configurado com as dependencias

---
+++ src/main/java/br/com/uvass/desafiosb/DesafiosbApplication.java
@@ -0,0 +1,13 @@
+package br.com.uvass.desafiosb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafiosbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesafiosbApplication.class, args);
+    }
+
+}
